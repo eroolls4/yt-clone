cacd62864527cf6014932458e92851e1fb021376
eroolls4 <eroollsaqipii@gmail.com>
2024-10-04 20:56:00 +0200

    fetching from yt api

---
+++ src/components/VideoCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const VideoCard = ({info}) => {
+
+    console.log(info)
+
+    const { snippet} = info;
+    const {publishedAt ,title ,channelTitle, thumbnails } = snippet;
+
+
+    return (
+        <div className="p-2 m-2 w-72 shadow-lg">
+            <img src={thumbnails.medium.url} />
+            <ul>
+                <li>Date published : {publishedAt}</li>
+                <li>Video title : {title}</li>
+                <li>Channel title :{channelTitle}</li>
+            </ul>
+
+
+        </div>
+    );
+}
+
+export default VideoCard;
--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import VideoCard from "./VideoCard";
 
 const VideoContainer = () => {
+    const [videos,setVideos] = useState([]);
+
+    useEffect(() => {
+        fetchVideos();
+    }, []);
+
+    const fetchVideos = async () => {
+
+        // console.log(process.env.YOUTUBE_API_KEY)
+
+        const data = await fetch('https://youtube.googleapis.com/youtube/v3/search?part=snippet&location=21.5922529%2C-158.1147114&locationRadius=10mi&q=surfing&type=video&maxResults=50&key=AIzaSyB6UR7Bd12IJxM7ntXhbkTpAsR5331JNYw')
+        const json = await data.json()
+
+        // console.log(json)
+        setVideos(json.items)
+    }
+
+
     return (
-        <div>VideoContainer</div>
+        <div className="flex flex-wrap">
+
+            {videos.map((video) => (
+                <VideoCard key={video.id.videoId} info={video} />
+            ))};
+
+
+        </div>
     );
 }
 
